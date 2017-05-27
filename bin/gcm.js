42465ce1e8981f1ce41e6e517317d0287449241e
huyuqiong <921628437@qq.com>
2017-05-27 16:45:07 +0800

    d

---
+++ bin/gcm.js
@@ -0,0 +1,34 @@
+/**
+ * Created by hyq on 2016/7/14.
+ */
+
+var onNotification = function (notification) {
+    console.log('Event Received: ' + notification);
+};
+
+export default function (senderId, uuid, callback) {
+    if (window.GcmPushPlugin !== undefined) {
+        window.GcmPushPlugin.register(function (result) {
+            let params = {};
+            let update = {
+                token: result.gcm
+            };
+            let query = {
+                deviceId: uuid,
+                deviceType: 'android'
+            };
+            params.query = JSON.stringify(query);
+            console.log(result.gcm);
+            if (callback) {
+                callback(params);
+            }else {
+                return Promise.resolve(params);
+            }
+        }, function (error) {
+            console.log('Error: ' + error);
+        }, {
+            'senderId': senderId,
+            'jsCallback': 'onNotification'
+        });
+    }
+};
